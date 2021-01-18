a28f0e13e04aca03ddf4bb531cef22cff5878a74
Alex Korzh <alexkorzh7@gmail.com>
2021-01-19 01:29:18 +0300

    Wrap entire app with service provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { container, containerModule } from 'services/container'
+import { ServiceProvider } from 'services/provider'
+
+container.load(containerModule)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ServiceProvider container={container}>
+      <Component {...pageProps} />
+    </ServiceProvider>
+  )
 }
 
 export default MyApp
