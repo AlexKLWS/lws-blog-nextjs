4687826f30ab660793ef5fdfaded970f2885ae24
Alex Korzh <alexkorzh7@gmail.com>
2021-01-21 14:28:21 +0300

    Add default layout wrapper to file upload view

--- pages/secret/file-upload/FileUploadView.tsx
+++ pages/secret/file-upload/FileUploadView.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import FileUploadWidget from 'components/FileUploadWidget'
+import DefaultLayoutWrapper from 'components/DefaultLayoutWrapper/DefaultLayoutWrapper'
 
 const FileUploadView = () => {
   return (
-    <div>
-      <h1 className='App-title'>Upload Files</h1>
-      <FileUploadWidget />
-    </div>
+    <DefaultLayoutWrapper>
+      <div>
+        <h1 className='App-title'>Upload Files</h1>
+        <FileUploadWidget />
+      </div>
+    </DefaultLayoutWrapper>
   )
 }
 
