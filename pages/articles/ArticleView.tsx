7d5c91d98f3becce50bf4877fae58bf8cea65618
Alex Korzh <alexkorzh7@gmail.com>
2021-01-22 01:55:42 +0300

    Update article view to use simpler animation + fix

--- pages/articles/ArticleView.tsx
+++ pages/articles/ArticleView.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import { useTransition, animated } from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 import { DateTime } from 'luxon'
 
 import { Article } from 'types/materials'
@@ -20,12 +20,14 @@ const LoadableArticleTextView = dynamic(() => import('../../components/ArticleTe
 })
 
 export const ArticleView: React.FC<Props> = ({ article }: Props) => {
-  const transitions = useTransition(!!article, null, {
-    config: { tension: 280, friction: 90 },
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  })
+  const [style, animate] = useSpring(
+    {
+      config: { tension: 280, friction: 90 },
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    },
+    [],
+  )
   const formattedDate = !!article && DateTime.fromISO(article.createdAt!).toLocaleString(DateTime.DATE_SHORT)
 
   return (
@@ -36,32 +38,14 @@ export const ArticleView: React.FC<Props> = ({ article }: Props) => {
       </Head>
       <DefaultLayoutWrapper>
         <div className={styles.ArticleContainer}>
-          {transitions.map(({ item, key, props }) =>
-            !item ? (
-              <animated.div
-                key={key}
-                style={{ ...props, padding: '20px 42px', position: 'absolute', right: 0, left: 0 }}
-              >
-                <div className={styles.ArticleTitleContainer}>
-                  <div className={styles.ArticleTitleSkeletonLoader} />
-                  <div className={styles.ArticleSubtitleSkeletonLoader} />
-                </div>
-                <div className={styles.ArticleTextSkeletonLoader} />
-              </animated.div>
-            ) : (
-              <animated.div
-                key={key}
-                style={{ ...props, padding: '20px 42px', minHeight: '720px', position: 'relative' }}
-              >
-                <div className={styles.ArticleTitleContainer}>
-                  <h1 className={styles.ArticleTitle}>{article?.name}</h1>
-                  <h2 className={styles.ArticleSubtitle}>{article?.subtitle}</h2>
-                </div>
-                <LoadableArticleTextView text={article!.articleText} />
-                <p className={styles.ArticleDate}>{formattedDate}</p>
-              </animated.div>
-            ),
-          )}
+          <animated.div style={{ ...style, padding: '20px 42px', minHeight: '720px', position: 'relative' }}>
+            <div className={styles.ArticleTitleContainer}>
+              <h1 className={styles.ArticleTitle}>{article?.name}</h1>
+              <h2 className={styles.ArticleSubtitle}>{article?.subtitle}</h2>
+            </div>
+            <LoadableArticleTextView text={article!.articleText} />
+            <p className={styles.ArticleDate}>{formattedDate}</p>
+          </animated.div>
         </div>
       </DefaultLayoutWrapper>
     </>
