19490cbcee08774b2b2f0a22c3f84808c1c10c06
Alex Korzh <alexkorzh7@gmail.com>
2021-01-23 20:49:59 +0300

    Fix react spring prod error

--- next.config.js
+++ next.config.js
@@ -4,13 +4,19 @@ module.exports = {
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        issuer: {
+          test: /\.(js|ts)x?$/,
+        },
+        use: ['@svgr/webpack'],
       },
-      use: ['@svgr/webpack'],
-    })
+      {
+        test: /react-spring/,
+        sideEffects: true,
+      },
+    )
 
     return config
   },
