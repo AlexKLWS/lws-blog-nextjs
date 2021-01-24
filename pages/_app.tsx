70a86725ca8fde627b4d8ad362033e86d41a242d
Alex Korzh <alexkorzh7@gmail.com>
2021-01-24 12:22:43 +0300

    Change _app to typescript, add loading indicator on route change

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import '../styles/globals.scss'
+
+import { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import { container, containerModule } from 'services/container'
+import { ServiceProvider } from 'services/provider'
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+container.load(containerModule)
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ServiceProvider container={container}>
+      <Component {...pageProps} />
+    </ServiceProvider>
+  )
+}
+
+export default MyApp
