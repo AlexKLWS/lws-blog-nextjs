6ac05474634a4a87f58a408381729185f66db5e2
Alex Korzh <alexkorzh7@gmail.com>
2021-01-29 00:24:43 +0300

    Change login view privacy text

--- pagesComponents/login/LoginView.tsx
+++ pagesComponents/login/LoginView.tsx
@@ -53,11 +53,10 @@ const LoginView: React.FC<Props> = (props: Props) => {
           value='Log in'
         />
         <p className={styles.LoginGDPRInfo}>
-          I've made this login page just for myself, so I don't expect anyone else to use it. Still, I'm providing this
-          notice just in case. This website is using <a href='https://en.wikipedia.org/wiki/HTTP_cookie'>cookies</a> to
-          store a <a href='https://jwt.io/introduction/'>JWT token</a> used to authenticate the user. The token is
-          stored for 24 hours only. By logging in you consent to store this token on your device. If you had logged in
-          previously and would like to remove the token cookie, press the button below.
+          This website is using <a href='https://en.wikipedia.org/wiki/HTTP_cookie'>cookies</a> to store a
+          <a href='https://jwt.io/introduction/'>JWT token</a> used to authenticate the user. The token is stored for 24
+          hours only. By logging in you consent to store this token on your device. If you had logged in previously and
+          would like to remove the token cookie, press the button below.
         </p>
         <input className='App-button' onClick={props.onClearCookiesPress} type='submit' value='Clear Cookies' />
       </div>
