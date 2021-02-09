6915f2540d1dd3259d7a14f99ef305ff76a62f19
Alex Korzh <alexkorzh7@gmail.com>
2021-02-09 16:15:43 +0100

    Add api domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['longwintershadows.com'],
+    domains: ['localhost', 'longwintershadows.com', 'api.longwintershadows.com'],
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   webpack(config) {
