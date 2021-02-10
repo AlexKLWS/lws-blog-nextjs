048576da73924b55a232d3843af724686d3b8d30
Alex Korzh <alexkorzh7@gmail.com>
2021-02-10 18:23:41 +0100

    Add ability to view secret materials by category

---
+++ pages/secret/guides/[id].ts
@@ -0,0 +1,7 @@
+import GuideController, {
+  getServerSideProps as exportedGetServerSideProps,
+} from 'pagesComponents/secret/guide-editor/GuideEditorController'
+
+export const getServerSideProps = exportedGetServerSideProps
+
+export default GuideController
---
+++ pages/secret/guides/index.ts
@@ -0,0 +1,7 @@
+import HomeController, {
+  getServerSideProps as exportedGetServerSideProps,
+} from 'pagesComponents/secret/home/HomeController'
+
+export const getServerSideProps = exportedGetServerSideProps
+
+export default HomeController
