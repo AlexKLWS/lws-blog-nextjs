8a8b0045aa0173f99002090aa0da0007ddfadbc8
Alex Korzh <alexkorzh7@gmail.com>
2021-04-18 18:35:51 +0200

    Add games category pages

---
+++ pages/games/index.ts
@@ -0,0 +1,5 @@
+import HomeController, { getServerSideProps as exportedGetServerSideProps } from 'pagesComponents/home/HomeController'
+
+export const getServerSideProps = exportedGetServerSideProps
+
+export default HomeController
