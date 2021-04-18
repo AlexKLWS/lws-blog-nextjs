6ee99a01a2c52e078b75a423045ee766d3e17dfa
Alex Korzh <alexkorzh7@gmail.com>
2021-04-18 20:10:52 +0200

    Add games to header and make it switch to dropdown for smaller screens

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Image from 'next/image'
 import { useMediaQuery } from 'react-responsive'
 import { useRouter } from 'next/router'
@@ -7,6 +7,7 @@ import styles from './Header.module.scss'
 import routes from 'consts/routes'
 import { isSmallerScreenQuery } from 'consts/media'
 import LinkWithStyles from 'components/LinkWithStyles'
+import Dropdown from 'components/Dropdowns/Dropdown/Dropdown'
 
 type Props = {
   isSecret?: boolean
@@ -17,6 +18,60 @@ const Header: React.FC<Props> = (props: Props) => {
   const isSmallerScreen = useMediaQuery({
     query: isSmallerScreenQuery,
   })
+
+  const dropdownLabels = useMemo(() => {
+    const labels = new Map()
+    labels.set(routes.secret.life, '> Life')
+    labels.set(routes.life, '> Life')
+    labels.set(routes.secret.code, '> Code')
+    labels.set(routes.code, '> Code')
+    labels.set(routes.secret.guides, '> Guides')
+    labels.set(routes.guides, '> Guides')
+    labels.set(routes.secret.projects, '> Projects')
+    labels.set(routes.projects, '> Projects')
+    labels.set(routes.secret.games, '> Games')
+    labels.set(routes.games, '> Games')
+    labels.set(routes.secret.home, '> Topic')
+    labels.set(routes.home, '> Topic')
+    return labels
+  }, [])
+
+  const dropdownItems = useMemo(
+    () => [
+      {
+        label: 'Life',
+        callback: () => {
+          router.push(props.isSecret ? routes.secret.life : routes.life)
+        },
+      },
+      {
+        label: 'Code',
+        callback: () => {
+          router.push(props.isSecret ? routes.secret.code : routes.code)
+        },
+      },
+      {
+        label: 'Guides',
+        callback: () => {
+          router.push(props.isSecret ? routes.secret.guides : routes.guides)
+        },
+      },
+      {
+        label: 'Projects',
+        callback: () => {
+          router.push(props.isSecret ? routes.secret.projects : routes.projects)
+        },
+      },
+      {
+        label: 'Games',
+        callback: () => {
+          router.push(props.isSecret ? routes.secret.games : routes.games)
+        },
+      },
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  )
 
   const getLinkStyle = (route: string) => {
     const currentRoute = router.pathname
@@ -41,32 +96,44 @@ const Header: React.FC<Props> = (props: Props) => {
           )}
         </div>
       </div>
-      <nav className={styles.SectionsList}>
-        <LinkWithStyles
-          href={props.isSecret ? routes.secret.life : routes.life}
-          className={getLinkStyle(props.isSecret ? routes.secret.life : routes.life)}
-        >
-          Life
-        </LinkWithStyles>
-        <LinkWithStyles
-          href={props.isSecret ? routes.secret.code : routes.code}
-          className={getLinkStyle(props.isSecret ? routes.secret.code : routes.code)}
-        >
-          Code
-        </LinkWithStyles>
-        <LinkWithStyles
-          href={props.isSecret ? routes.secret.guides : routes.guides}
-          className={getLinkStyle(props.isSecret ? routes.secret.guides : routes.guides)}
-        >
-          Guides
-        </LinkWithStyles>
-        <LinkWithStyles
-          href={props.isSecret ? routes.secret.projects : routes.projects}
-          className={getLinkStyle(props.isSecret ? routes.secret.projects : routes.projects)}
-        >
-          Projects
-        </LinkWithStyles>
-      </nav>
+      {isSmallerScreen ? (
+        <div className={styles.DropdownContainer}>
+          <Dropdown dropdownTriggerText={dropdownLabels.get(router.pathname)} items={dropdownItems} />
+        </div>
+      ) : (
+        <nav className={styles.SectionsList}>
+          <LinkWithStyles
+            href={props.isSecret ? routes.secret.life : routes.life}
+            className={getLinkStyle(props.isSecret ? routes.secret.life : routes.life)}
+          >
+            Life
+          </LinkWithStyles>
+          <LinkWithStyles
+            href={props.isSecret ? routes.secret.code : routes.code}
+            className={getLinkStyle(props.isSecret ? routes.secret.code : routes.code)}
+          >
+            Code
+          </LinkWithStyles>
+          <LinkWithStyles
+            href={props.isSecret ? routes.secret.guides : routes.guides}
+            className={getLinkStyle(props.isSecret ? routes.secret.guides : routes.guides)}
+          >
+            Guides
+          </LinkWithStyles>
+          <LinkWithStyles
+            href={props.isSecret ? routes.secret.projects : routes.projects}
+            className={getLinkStyle(props.isSecret ? routes.secret.projects : routes.projects)}
+          >
+            Projects
+          </LinkWithStyles>
+          <LinkWithStyles
+            href={props.isSecret ? routes.secret.games : routes.games}
+            className={getLinkStyle(props.isSecret ? routes.secret.games : routes.games)}
+          >
+            Games
+          </LinkWithStyles>
+        </nav>
+      )}
     </div>
   )
 }
