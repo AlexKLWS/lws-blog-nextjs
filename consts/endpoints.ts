ee819f20235f700a8b251af2b9c927cd7061448c
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 01:43:39 +0200

    Move open graph image to meta defaults

--- consts/endpoints.ts
+++ consts/endpoints.ts
@@ -1,4 +1,3 @@
 export const baseURL = process.env.NEXT_PUBLIC_BASE_API_URL
 export const authEndpoint = `${baseURL}/auth`
 export const apiEndpoint = `${baseURL}/api`
-export const openGraphImage = `${baseURL}/og_image.png`
--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,3 +1,6 @@
+import { baseURL } from './endpoints'
+
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
 export const DEFAULT_TITLE = 'Long Winter Shadows'
 export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
+export const OPEN_GRAPH_IMAGE = `${baseURL}/og_image.png`
