e182980d03cafa37a8d92f8a822c24017a842550
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 01:40:08 +0200

    Add open graph media image url to endpoints, and author name to meta defaults

--- consts/endpoints.ts
+++ consts/endpoints.ts
@@ -1,3 +1,4 @@
 export const baseURL = process.env.NEXT_PUBLIC_BASE_API_URL
 export const authEndpoint = `${baseURL}/auth`
 export const apiEndpoint = `${baseURL}/api`
+export const openGraphImage = `${baseURL}/og_image.png`
--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
 export const DEFAULT_TITLE = 'Long Winter Shadows'
+export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
