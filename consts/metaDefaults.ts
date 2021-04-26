587e87bc76979bb0b5c0e6b4d2177f31715299d1
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 02:13:57 +0200

    Fix open graph image default const

--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,6 +1,4 @@
-import { baseURL } from './endpoints'
-
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
 export const DEFAULT_TITLE = 'Long Winter Shadows'
 export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
-export const OPEN_GRAPH_IMAGE = `${baseURL}/og_image.png`
+export const OPEN_GRAPH_IMAGE = typeof window !== 'undefined' && window.location.origin + '/og_image.png'
