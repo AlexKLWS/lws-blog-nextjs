b73e4d652dcc4552cc5e0563c79eee6388c83341
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 02:24:47 +0200

    Make sure open graph image is undefined instead of false

--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
 export const DEFAULT_TITLE = 'Long Winter Shadows'
 export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
-export const OPEN_GRAPH_IMAGE = typeof window !== 'undefined' && window.location.origin + '/og_image.png'
+export const OPEN_GRAPH_IMAGE = (typeof window !== 'undefined' && window.location.origin + '/og_image.png') || undefined
