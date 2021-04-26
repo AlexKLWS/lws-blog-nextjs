3ca7a38004071f6569183f220e48e197d9b90339
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 21:17:03 +0200

    Update 404 page meta title

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Head from 'next/head'
 
 import FullscreenMessageView from 'components/FullscreenMessageView/FullscreenMessageView'
-import { DEFAULT_DESCRIPTION } from 'consts/metaDefaults'
+import { DEFAULT_AUTHOR_NAME, DEFAULT_DESCRIPTION, DEFAULT_TITLE } from 'consts/metaDefaults'
 
 const EmptyPage: React.FC = () => {
   return (
     <>
       <Head>
-        <title>LWS - Alex Korzh - 404</title>
+        <title>{`${DEFAULT_TITLE} - ${DEFAULT_AUTHOR_NAME} - 404`}</title>
         <meta name='description' content={DEFAULT_DESCRIPTION} />
         <link rel='icon' href='/favicon.ico' />
       </Head>
