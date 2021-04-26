70d9257db522214e87436f43000060ff22c9b855
Alex Korzh <alexkorzh7@gmail.com>
2021-04-26 21:07:56 +0200

    Tweak _document meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,12 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html lang='en'>
         <Head>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <script async defer data-domain='longwintershadows.com' src='https://plausible.io/js/plausible.js'></script>
         </Head>
         <body style={{ backgroundColor: '#000', marginTop: '0px' }}>
