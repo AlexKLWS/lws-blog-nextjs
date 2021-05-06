8080c914a86ea862504310686624bfb27e16942d
Alex Korzh <alexkorzh7@gmail.com>
2021-05-07 01:33:34 +0200

    Remove viewport from _document to avoid warnings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta charSet='utf-8' />
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <script async defer data-domain='longwintershadows.com' src='https://plausible.io/js/plausible.js'></script>
         </Head>
         <body style={{ backgroundColor: '#000', marginTop: '0px' }}>
