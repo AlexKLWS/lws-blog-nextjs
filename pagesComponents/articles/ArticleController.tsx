e63abadcb0d47c775be6e7d39095ba2f9ffff543
Alex Korzh <alexkorzh7@gmail.com>
2021-05-07 01:34:01 +0200

    Use proper fullUrl for article controller (test)

--- pagesComponents/articles/ArticleController.tsx
+++ pagesComponents/articles/ArticleController.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import React, { useEffect } from 'react'
 import { GetServerSideProps } from 'next'
+import absoluteUrl from 'next-absolute-url'
 
 import ArticleView from './ArticleView'
 import { serverSideArticleClient } from 'facades/materialClientFacade'
 import FullscreenMessageView from 'components/FullscreenMessageView/FullscreenMessageView'
 import { Article } from 'types/materials'
-import { baseURL } from 'consts/endpoints'
 import { DEFAULT_AUTHOR_NAME, DEFAULT_DESCRIPTION, DEFAULT_TITLE, OPEN_GRAPH_IMAGE } from 'consts/metaDefaults'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -17,14 +17,28 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
   const response = await fetchArticle((context.params['id'] as string) || '')
+  let fullUrl
+  if (context.req) {
+    // Server side rendering
+    const { origin } = absoluteUrl(context.req)
+    fullUrl = origin + context.req.url
+  } else {
+    // Client side rendering
+    fullUrl =
+      window.location.protocol +
+      '//' +
+      window.location.hostname +
+      (window.location.port ? ':' + window.location.port : '')
+  }
   return {
-    props: { article: response.material, error: response.error },
+    props: { article: response.material, error: response.error, fullUrl },
   }
 }
 
 type Props = {
   article: Article
   error?: Error
+  fullUrl: string
 }
 
 const ArticleController: React.FC<Props> = (props: Props) => {
@@ -62,7 +76,7 @@ const ArticleController: React.FC<Props> = (props: Props) => {
         <title>{getMetaTitle()}</title>
         <link rel='icon' href='/favicon.ico' />
         <meta property='og:image' content={OPEN_GRAPH_IMAGE} />
-        <meta property='og:url' content={baseURL} />
+        <meta property='og:url' content={props.fullUrl} />
         <meta property='og:type' content='website' />
         <meta property='og:image:height' content='630' />
         <meta property='og:image:width' content='1200' />
