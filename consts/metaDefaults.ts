7e332e14a5000b67e44083acc36815a08a55413e
Alex Korzh <alexkorzh7@gmail.com>
2021-05-07 01:33:15 +0200

    Serve OG image from backend service instead of vercel

--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,4 +1,6 @@
+import { baseURL } from './endpoints'
+
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
 export const DEFAULT_TITLE = 'Long Winter Shadows'
 export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
-export const OPEN_GRAPH_IMAGE = (typeof window !== 'undefined' && window.location.origin + '/og_image.png') || undefined
+export const OPEN_GRAPH_IMAGE = baseURL + '/assets/OG/og_image.png'
