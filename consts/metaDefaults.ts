7520d0d50854eadaa8b5ddaac1dc621a9279149b
Alex Korzh <alexkorzh7@gmail.com>
2021-05-08 15:16:58 +0200

    Change meta defaults default title

--- consts/metaDefaults.ts
+++ consts/metaDefaults.ts
@@ -1,6 +1,6 @@
 import { baseURL } from './endpoints'
 
 export const DEFAULT_DESCRIPTION = 'Personal blog by Alex Korzh'
-export const DEFAULT_TITLE = 'Long Winter Shadows'
+export const DEFAULT_TITLE = 'LWS'
 export const DEFAULT_AUTHOR_NAME = 'Alex Korzh'
 export const OPEN_GRAPH_IMAGE = baseURL + '/assets/OG/og_image.png'
