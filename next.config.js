c09af697b9fa304f247488637328555118266380
Alex Korzh <alexkorzh7@gmail.com>
2021-07-22 01:49:52 +0200

    Upgrade next js to latest and clean up configuration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,38 @@
 module.exports = {
   images: {
-    domains: ['localhost', 'longwintershadows.com', 'api.longwintershadows.com'],
+    domains: [
+      'localhost',
+      'longwintershadows.com',
+      'api.longwintershadows.com',
+      'www.restoclub.ru',
+      'storage.yandexcloud.net',
+    ],
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   webpack(config) {
-    config.module.rules.push(
-      {
-        test: /\.svg$/,
-        issuer: {
-          test: /\.(js|ts)x?$/,
+    config.module.rules.push({
+      test: /\.svg$/,
+      oneOf: [
+        {
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                  plugins: [{ removeViewBox: false }],
+                },
+                titleProp: true,
+              },
+            },
+          ],
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+          },
         },
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /react-spring/,
-        sideEffects: true,
-      },
-    )
+      ],
+    })
 
     return config
   },
