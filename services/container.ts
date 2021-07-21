e74c1068c5e8aeed63be9a1137848b2bda49bac6
Alex Korzh <alexkorzh7@gmail.com>
2021-07-21 22:49:47 +0200

    Remove fileupload service from container

--- services/container.ts
+++ services/container.ts
@@ -2,13 +2,11 @@ import 'reflect-metadata'
 import { Container, interfaces, ContainerModule } from 'inversify'
 import { SessionService, SessionServiceId, ISessionService } from './session'
 import { MaterailClientService, MaterialClientServiceId, IMaterialClientService } from './materialClient'
-import { FileUploadService, IFileUploadService, FileUploadServiceId } from './fileUpload'
 import { Material } from 'types/materials'
 
 export const containerModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<ISessionService>(SessionServiceId).to(SessionService).inSingletonScope()
   bind<IMaterialClientService<Material>>(MaterialClientServiceId).to(MaterailClientService)
-  bind<IFileUploadService>(FileUploadServiceId).to(FileUploadService)
 })
 
 export const container = new Container()
